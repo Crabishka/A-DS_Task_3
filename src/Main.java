6e17c51caa45fc7e21bb3c532a214c57c0dba1c4
Lenovo <hagoga.pr@gmail.com>
2021-03-18 13:08:32 +0300

    добавить рекурсию при false, которая возвращает очередь в начальное положение

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,26 @@ public class Main {
 
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
+
         q.offer(1);
-        q.offer(2);
+        q.offer(3);
         q.offer(3);
         q.offer(4);
         q.offer(3);
         q.offer(2);
         q.offer(1);
-        if (isQueuePalindrome(q)) System.out.println("DA");
+
+        /*
+        q.offer(1);
+        q.offer(3);
+        q.offer(3);
+        q.offer(4);
+        q.offer(3);
+        q.offer(2);
+        q.offer(1);
+         */
+        if (isQueuePalindrome(q)) System.out.println("yes");
+        else System.out.println("no");
 
 
     }
@@ -21,16 +33,25 @@ public class Main {
         return recFunc(queue.size() / 2, queue);
     }
 
-    public static boolean recFunc(int level, Queue<Integer> queue) {
+    public static boolean recFunc(int level, Queue<Integer> queue) throws NullPointerException {
+        int temp = 0;
         if (queue.size() % 2 == 1 && level == 0) queue.offer(queue.poll());
-        if (level > 0) {
-            int temp = queue.poll();
+        else {
+            temp = queue.poll();
             queue.offer(temp);
-            recFunc(level - 1, queue);
-            if (queue.poll() == temp) return true;
         }
-        return false;
+        if (level > 0) {
+            if (!recFunc(level - 1, queue)) return false;
+            if (queue.poll() == temp) {
+                queue.offer(temp);
+                return true;
+            } else {
+                return false;
+            }
 
 
+        }
+
+        return true;
     }
 }
